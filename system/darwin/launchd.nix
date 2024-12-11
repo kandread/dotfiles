a225412d1929b4d350f62da47b6f9b7bb4a5691a
Kostas Andreadis <kandread@umass.edu>
2024-12-11 13:06:05 -0500

    Updated launchd services

--- system/darwin/launchd.nix
+++ system/darwin/launchd.nix
@@ -2,21 +2,30 @@
 
 {
 
-  launchd.agents = {
-    mbsync = {
-      enable = true;
-      config = {
-        StartInterval = 300;
-        Program = "${pkgs.isync}/bin/mbsync";
-        ProgramArguments = ["--all" "--verbose"];
+  launchd.user.agents = {
+    isync = {
+      command = "${pkgs.isync}/bin/mbsync -a";
+      serviceConfig = {
+        ProcessType = "Background";
+        LowPriorityIO = true;
+        StartInterval = 5 * 60;
+        RunAtLoad = true;
+        KeepAlive = false;
+        StandardOutPath = "/dev/null";
+        StandardErrorPath = "/dev/null";
+        EnvironmentVariables = {
+          "SSL_CERT_FILE" = "/etc/ssl/certs/ca-certificates.crt";
+        };
       };
     };
     mu-index = {
-      enable = true;
-      config = {
-        StartInterval = 300;
-        Program = "${pkgs.mu}/bin/mu"
-          ProgramArguments = ["index"];
+      command = "${pkgs.mu}/bin/mu index";
+      serviceConfig = {
+        ProcessType = "Background";
+        LowPriorityIO = true;
+        StartInterval = 5 * 60 + 30;
+        RunAtLoad = true;
+        KeepAlive = false;
       };
     };
   };
